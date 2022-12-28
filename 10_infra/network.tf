07d1213a4326fd07ebc4bc3706e2f297609f82c6
kensuke goto <kensuke.goto.1981@gmail.com>
2022-12-28 11:03:52 +0900

    エラー修正

--- 10_infra/network.tf
+++ 10_infra/network.tf
@@ -2,7 +2,7 @@
 # VPC
 # ---------------------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_blockxx                     = "192.168.0.0/20"
+  cidr_block                       = "192.168.0.0/20"
   instance_tenancy                 = "default"
   enable_dns_support               = true
   enable_dns_hostnames             = true
